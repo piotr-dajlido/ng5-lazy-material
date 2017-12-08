21b3b0ec29823cd0d222ccd6a19301f5859a8c54
pdajlido <piotr.dajlido@euro.com.pl>
2017-12-08 02:37:56 +0100

    Created basic login form component for futher lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { MaterialModule } from '../material.module';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, // Required for Angular Material
+    // NoopAnimationsModule, // Fallback for Angular Material in unsupported browsers
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
